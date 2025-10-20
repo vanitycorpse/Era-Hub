50976ebe5452684786670a6813fb420f10698868
vanity corpse <141065176+vanitycorpse@users.noreply.github.com>
2025-10-20 21:03:28 +0300

    Update bootstrapper.lua

--- bootstrapper.lua
+++ bootstrapper.lua
@@ -1,12 +1,1 @@
-local socketAliases = {
-    "websocket",
-    "Websocket",
-    "WebSocket",
-    "web_socket"
-}
-
-for _, alias in socketAliases do
-    getgenv()[alias] = nil
-end
-
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/897e06c9c0723c26f17fb1172f6340ec.lua"))()
