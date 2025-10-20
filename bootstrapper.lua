a5000ae0f3609428aa266c91418516744ce78112
vanity corpse <141065176+vanitycorpse@users.noreply.github.com>
2025-10-20 21:17:38 +0300

    Update bootstrapper.lua

--- bootstrapper.lua
+++ bootstrapper.lua
@@ -1,1 +1,4 @@
+print("BOOTSRAPPER LOADED")
+print("LOADING LUARMOR")
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/897e06c9c0723c26f17fb1172f6340ec.lua"))()
+print("REQUSTED LUARMOR")
